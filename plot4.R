076a068b3459529f2fd297d1871e9c5f8d8237e9
Charley Cowens <charley.cowens@gmail.com>
2016-01-10 23:55:43 -0800

    adding 4

---
+++ plot4.R
@@ -0,0 +1,70 @@
+library(lubridate)
+
+project_data <- read.csv("project_data.csv", stringsAsFactors = FALSE)
+project_data$DateTime <- dmy_hms(paste(project_data$Date, project_data$Time))
+
+png("plot4.png")
+
+par(mfrow = c(2,2))
+
+with(project_data, {
+  plot(DateTime, Sub_metering_1, type="n", 
+       fg = "black",
+       xlab = "",
+       ylab = "Energy sub metering")
+  lines(DateTime, Sub_metering_1, 
+        col = "black")
+  lines(DateTime, Sub_metering_2, 
+        col = "red")
+  lines(DateTime, Sub_metering_3, 
+        col = "blue")
+  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
+         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+})
+
+with(project_data, {
+  plot(DateTime, Sub_metering_1, type="n", 
+       fg = "black",
+       xlab = "",
+       ylab = "Energy sub metering")
+  lines(DateTime, Sub_metering_1, 
+        col = "black")
+  lines(DateTime, Sub_metering_2, 
+        col = "red")
+  lines(DateTime, Sub_metering_3, 
+        col = "blue")
+  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
+         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+})
+
+with(project_data, {
+  plot(DateTime, Sub_metering_1, type="n", 
+       fg = "black",
+       xlab = "",
+       ylab = "Energy sub metering")
+  lines(DateTime, Sub_metering_1, 
+        col = "black")
+  lines(DateTime, Sub_metering_2, 
+        col = "red")
+  lines(DateTime, Sub_metering_3, 
+        col = "blue")
+  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
+         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+})
+
+with(project_data, {
+  plot(DateTime, Sub_metering_1, type="n", 
+       fg = "black",
+       xlab = "",
+       ylab = "Energy sub metering")
+  lines(DateTime, Sub_metering_1, 
+        col = "black")
+  lines(DateTime, Sub_metering_2, 
+        col = "red")
+  lines(DateTime, Sub_metering_3, 
+        col = "blue")
+  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
+         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+})
+
+dev.off()
