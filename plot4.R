79aa5fdebdb4e70b2580f6d9bb77a53b0188c7dd
Charley Cowens <charley.cowens@gmail.com>
2016-01-11 00:07:45 -0800

    loose ends

--- plot4.R
+++ plot4.R
@@ -8,18 +8,21 @@ png("plot4.png")
 par(mfrow = c(2,2))
 
 with(project_data, {
-  plot(DateTime, Sub_metering_1, type="n", 
+  plot(DateTime, Global_active_power, type="n", 
        fg = "black",
        xlab = "",
-       ylab = "Energy sub metering")
-  lines(DateTime, Sub_metering_1, 
+       ylab = "Global Active Power")
+  lines(DateTime, Global_active_power, 
         col = "black")
-  lines(DateTime, Sub_metering_2, 
-        col = "red")
-  lines(DateTime, Sub_metering_3, 
-        col = "blue")
-  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
-         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+})
+
+with(project_data, {
+  plot(DateTime, Voltage, type="n", 
+       fg = "black",
+       xlab = "datetime",
+       ylab = "Voltage")
+  lines(DateTime, Voltage, 
+        col = "black")
 })
 
 with(project_data, {
@@ -38,33 +41,12 @@ with(project_data, {
 })
 
 with(project_data, {
-  plot(DateTime, Sub_metering_1, type="n", 
+  plot(DateTime, Global_reactive_power, type="n", 
        fg = "black",
-       xlab = "",
-       ylab = "Energy sub metering")
-  lines(DateTime, Sub_metering_1, 
+       xlab = "datetime",
+       ylab = "Global_reactive_power")
+  lines(DateTime, Global_reactive_power, 
         col = "black")
-  lines(DateTime, Sub_metering_2, 
-        col = "red")
-  lines(DateTime, Sub_metering_3, 
-        col = "blue")
-  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
-         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
-})
-
-with(project_data, {
-  plot(DateTime, Sub_metering_1, type="n", 
-       fg = "black",
-       xlab = "",
-       ylab = "Energy sub metering")
-  lines(DateTime, Sub_metering_1, 
-        col = "black")
-  lines(DateTime, Sub_metering_2, 
-        col = "red")
-  lines(DateTime, Sub_metering_3, 
-        col = "blue")
-  legend("topright", col = c("black", "red", "blue"), text.col = "black", lty=c(1,1,1), 
-         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
 })
 
 dev.off()
