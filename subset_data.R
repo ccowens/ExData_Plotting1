36412f92a3ca8c435f3c211ada9cddaf544f6b3f
Charley Cowens <charley.cowens@gmail.com>
2016-01-10 21:51:57 -0800

    Subsetting First

---
+++ subset_data.R
@@ -0,0 +1,13 @@
+# read in the file I unzipped from downloading:
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# as a dataframe
+# converting the ?'s to NAs
+read_in <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", quote = "\"",
+                      dec = ".", fill = TRUE, comment.char = "", stringsAsFactors = FALSE, na.string=c("?"))
+
+# iust the use the string values under Date to get the rows that are required
+project_data <- subset(read_in, Date %in% c("1/2/2007","2/2/2007"))
+rm(read_in)
+
+# write out the subsetted file for use in other scripts 
+write.csv(project_data, "project_data.csv", row.names = FALSE)
